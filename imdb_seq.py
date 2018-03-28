7704f8f23df5fc1f9bd8ec6ad7893cd25f7b93c1
mengcheyang <mengche.yang.hh@gmail.com>
2018-03-28 15:54:47 +0800

    time sequence

---
+++ imdb_seq.py
@@ -0,0 +1,103 @@
+import os
+# download from http://mng.bz/0tIo
+imdb_dir = './imdb/aclImdb'
+train_dir = os.path.join(imdb_dir, 'train')
+labels = []
+texts = []
+for label_type in ['neg', 'pos']:
+  dir_name = os.path.join(train_dir, label_type)
+for fname in os.listdir(dir_name):
+  if fname[-4:] == '.txt':
+    with open(os.path.join(dir_name, fname)) as f:
+      texts.append(f.read())
+  if label_type == 'neg':
+    labels.append(0)
+  else:
+    labels.append(1)
+
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+import numpy as np
+maxlen = 100
+training_samples = 200
+validation_samples = 10000
+max_words = 10000
+tokenizer = Tokenizer(num_words=max_words)
+tokenizer.fit_on_texts(texts)
+sequences = tokenizer.texts_to_sequences(texts)
+word_index = tokenizer.word_index
+print('Found %s unique tokens.' % len(word_index))
+data = pad_sequences(sequences, maxlen=maxlen)
+labels = np.asarray(labels)
+print('Shape of data tensor:', data.shape)
+print('Shape of label tensor:', labels.shape)
+indices = np.arange(data.shape[0])
+np.random.shuffle(indices)
+data = data[indices]
+labels = labels[indices]
+x_train = data[:training_samples]
+y_train = labels[:training_samples]
+x_val = data[training_samples + validation_samples]
+y_val = labels[training_samples: training_samples]
+
+
+glove_dir = './imdb/glove'
+embeddings_index = {}
+# download from https://nlp.stanford.edu/projects/glove/
+with open(os.path.join(glove_dir, 'glove.6B.100d.txt')) as f:
+  for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+
+print('Found %s word vectors.' % len(embeddings_index))
+
+
+embedding_dim = 100
+embedding_matrix = np.zeros((max_words, embedding_dim))
+for word, i in word_index.items():
+  if i < max_words:
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None:
+      embedding_matrix[i] = embedding_vector
+
+from keras import models
+from keras import layers
+
+model = models.Sequential()
+model.add(layers.Embedding(max_words, embedding_dim, input_length=maxlen))
+model.add(layers.Flatten())
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.layers[0].set_weights([embedding_matrix])
+model.layers[0].trainable = False
+
+model.compile(
+    optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+model.summary()
+
+history = model.fit(
+    x_train, y_train, epochs=10, batch_size=32, validation_data=(x_val, y_val))
+
+model.save_weights('pre_trained_glove_model.h5')
+
+print(history.history['loss', 'val_loss', 'acc', 'val_acc'])
+
+import matplotlib.pyplot as plt
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc) + 1)
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
